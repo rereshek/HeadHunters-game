e67ff7182ec24cc625891d844c5f3e17ee67414c
BronwenM <39170856+BronwenM@users.noreply.github.com>
2020-03-13 06:32:37 -0400

    Add files via upload

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Player player1;
+    public Player player2;
+
+    public float enemySpeed = 2.0f;
+    public int eHealth = 250;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        gameObject.GetComponent<Rigidbody2D>().drag = 0;
+        if (col.gameObject.CompareTag("Player1"))
+        {
+            //Debug.Log("Player detected");            
+            gameObject.GetComponent<Rigidbody2D>().AddForce((player1.transform.position - transform.position).normalized * enemySpeed);
+        }
+        if (col.gameObject.CompareTag("Player2"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce((player2.transform.position - transform.position).normalized * enemySpeed);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Debug.Log("lost player");
+        gameObject.GetComponent<Rigidbody2D>().drag = 6;
+    }
+
+    /*private void FixedUpdate()
+    {
+        Collider2D myCollider = gameObject.GetComponent<Collider2D>();
+        int numColliders = 2;
+        Collider2D[] colliders = new Collider2D[numColliders];
+        ContactFilter2D contactFilter = new ContactFilter2D();
+
+        int colliderCount = myCollider.OverlapCollider(contactFilter, colliders);
+
+        if (colliderCount == numColliders)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce((player.transform.position - transform.position).normalized * enemySpeed);
+        }
+        if (colliderCount < numColliders)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            gameObject.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+        }
+
+    }*/
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public float speed = 3.0f;
+    public int pHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(gameObject.name == "Player1")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector2.up * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector2.down * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * speed);
+            }
+        }
+        if(gameObject.name == "Player2")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector2.up * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector2.down * Time.deltaTime * speed);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * speed);
+            }
+        }
+        
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+
+        if (other.gameObject.name.Contains("Enemy")) {
+            Debug.Log("Hit");
+        }
+        if (gameObject.CompareTag("Edge"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+}
