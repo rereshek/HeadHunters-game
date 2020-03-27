3412fe202c760ce88c5e7209546cc9e3e785e711
BronwenM <39170856+BronwenM@users.noreply.github.com>
2020-03-27 14:20:05 -0700

    Add files via upload

---
+++ Scripts/Play1Move.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play1Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //public GameObject playerSlash;
+
+    //Speed of the player character
+    float moveSpeed = 3F;
+    float normalizedSpeed = 2.121F;
+    float x;
+    float y;
+
+    //direction faced, 0=up 1=right 2=down 3=left
+    int facedir = 0;
+
+    //attack timer
+    float actionTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = transform.position.x;
+        y = transform.position.y;
+
+        Vector3 translationDirection = Vector3.zero;
+
+        actionTimer += Time.deltaTime;
+        //if (Input.GetKey("w") && Input.GetKey("d"))
+        //{
+        //    transform.Translate(normalizedSpeed * Time.deltaTime, normalizedSpeed * Time.deltaTime, 0);
+        //}
+        //else if (Input.GetKey("s") && Input.GetKey("d"))
+        //{
+        //    transform.Translate(normalizedSpeed * Time.deltaTime, -normalizedSpeed * Time.deltaTime, 0);
+        //}
+        //else if (Input.GetKey("w") && Input.GetKey("a"))
+        //{
+        //    transform.Translate(-normalizedSpeed * Time.deltaTime, normalizedSpeed * Time.deltaTime, 0);
+        //}
+        //else if (Input.GetKey("s") && Input.GetKey("a"))
+        //{
+        //    transform.Translate(-normalizedSpeed * Time.deltaTime, -normalizedSpeed * Time.deltaTime, 0);
+        //}
+        //else
+        //{
+            if (Input.GetKey("w"))
+            {
+            //transform.Translate(0, moveSpeed * Time.deltaTime, 0);
+                translationDirection += Vector3.up;
+                facedir = 0;
+            }
+            if (Input.GetKey("s"))
+            {
+                translationDirection += Vector3.down;
+                //transform.Translate(0, -moveSpeed * Time.deltaTime, 0);
+                facedir = 2;
+            }
+            if (Input.GetKey("d"))
+            {
+                translationDirection += Vector3.right;
+                //transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+                facedir = 1;
+            }
+            if (Input.GetKey("a"))
+            {
+                translationDirection += Vector3.left;
+                //transform.Translate(-moveSpeed * Time.deltaTime, 0, 0);
+                facedir = 3;
+            }
+        transform.Translate(translationDirection.normalized * moveSpeed * Time.deltaTime);
+        //}
+        //if (Input.GetKeyDown("space"))
+        //{
+        //    if (actionTimer > 0.3)
+        //    {
+        //        GameObject slashClone;
+        //        slashClone = Instantiate(playerSlash, transform.position, Quaternion.identity);
+        //        slashClone.AddComponent<destroyScript>();
+        //        if (facedir == 0)
+        //        {
+        //            slashClone.GetComponent<Transform>().position = new Vector2(x, y + 1);
+        //        }
+        //        else if (facedir == 1)
+        //        {
+        //            slashClone.GetComponent<Transform>().position = new Vector2(x + 1, y);
+        //        }
+        //        else if (facedir == 2)
+        //        {
+        //            slashClone.GetComponent<Transform>().position = new Vector2(x, y - 1);
+        //        }
+        //        else if (facedir == 3)
+        //        {
+        //            slashClone.GetComponent<Transform>().position = new Vector2(x - 1, y);
+        //        }
+        //        actionTimer = 0;
+        //    }
+        //}
+
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+    public int pHealth = 100;
+    int pFoodCount = 0;
+    public int playerID;
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+        if (playerID == 1)
+        {
+            pFoodCount = GlobalDataManager.Instance.player1Food;
+            pHealth = GlobalDataManager.Instance.player1Health;
+            AddFood(0);
+        }
+        if (playerID == 2)
+        {
+            pFoodCount = GlobalDataManager.Instance.player2Food;
+            pHealth = GlobalDataManager.Instance.player2Health;
+            AddFood(0);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 newDirection = Vector2.zero;
+
+        if (Input.GetKey(up))
+        {
+            newDirection += Vector2.up;
+        }
+        if (Input.GetKey(right))
+        {
+            newDirection += Vector2.right;
+        }
+        if (Input.GetKey(down))
+        {
+            newDirection += Vector2.down;
+        }
+        if (Input.GetKey(left))
+        {
+            newDirection += Vector2.left;
+        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    EventSystem.Instance.FoodCollected(pFoodCount);
+        //    pFoodCount++;
+        //}
+        transform.Translate(newDirection.normalized * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.name.Contains("Enemy"))
+        {
+            Debug.Log("Hit");
+        }
+        if (gameObject.CompareTag("Edge"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    public void AddFood(int food)
+    {
+        FoodUIData newFoodData;
+        //Add food to the players count and update the UI
+        pFoodCount += food;
+        newFoodData.foodCount = pFoodCount;
+        newFoodData.playerID = playerID;
+        EventSystem.Instance.FoodCollected(newFoodData);
+
+    }
+
+    public void DropFood(int food)
+    {
+        //Delete food and update the UI
+        //Also need to stop fighting - tell the enemy as well so they pause while they pick up the food
+    }
+
+    public void SaveData()
+    {
+        if (playerID == 1)
+        {
+            GlobalDataManager.Instance.player1Food = pFoodCount;
+            GlobalDataManager.Instance.player1Health = pHealth;
+        }
+        if (playerID == 2)
+        {
+            GlobalDataManager.Instance.player2Food = pFoodCount;
+            GlobalDataManager.Instance.player2Health = pHealth;
+        }
+
+    }
+}
